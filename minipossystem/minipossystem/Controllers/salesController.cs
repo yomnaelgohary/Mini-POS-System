b5d3e93935f95e044a9d2976e29c38e803c54573
yomnaelgohary <yomnaelgohary011@gmail.com>
2025-07-07 10:10:31 +0300

    Initial commit of ASP.NET MVC project without build files

---
+++ minipossystem/minipossystem/Controllers/salesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace minipossystem.Controllers
+{
+    public class salesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
