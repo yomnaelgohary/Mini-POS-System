07eb235ac5c3961cc4edbdef74119214cb1c9297
yomnaelgohary <yomnaelgohary011@gmail.com>
2025-07-13 11:08:07 +0300

    Enable Sessions

---
+++ minipossystem/minipossystem/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using minipossystem.Models;
+using System;
+
+namespace minipossystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly MiniPosSystemContext context;
+
+        public LoginController(MiniPosSystemContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(int employeeId)
+        {
+            var employee = context.Employees.FirstOrDefault(e => e.EmployeeId == employeeId);
+
+            if (employee == null)
+            {
+                ViewBag.Error = "Employee not found.";
+                return View("Index");
+            }
+
+            var roleAccess = context.RoleAccesses.FirstOrDefault(r => r.RoleName == employee.EmployeeRole);
+
+            if (roleAccess == null)
+            {
+                ViewBag.Error = "Access role not found.";
+                return View("Index");
+            }
+
+            // Store info in session
+            HttpContext.Session.SetInt32("EmployeeId", employee.EmployeeId);
+            HttpContext.Session.SetString("RoleName", employee.EmployeeRole);
+
+            return RedirectToAction("Dashboard", "Home");
+        }
+    }
+
+}
--- minipossystem/minipossystem/Program.cs
+++ minipossystem/minipossystem/Program.cs
@@ -2,16 +2,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -19,6 +25,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
