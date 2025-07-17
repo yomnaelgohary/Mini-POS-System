b015c3fbdddc5a61f7967d914282ab7e04e7edde
yomnaelgohary <yomnaelgohary011@gmail.com>
2025-07-17 10:25:58 +0300

    Update an existing costumerinfo (trigger log trace in database)

--- InsertTrigger.sql
+++ InsertTrigger.sql
@@ -1,47 +1,81 @@
 -- insert into Costumer 
  --Values ('Yomna', '014')
 
- select*
- from Costumer 
+-- select*
+-- from Costumer 
 
- Create Table newCostumerAudit (
- AuditID INT IDENTITY(1,1) PRIMARY KEY,
- CostumerID int,
- CostumerName varchar(50),
- CostumerInfo varchar (50),
- );
+-- Create Table newCostumerAudit (
+-- AuditID INT IDENTITY(1,1) PRIMARY KEY,
+-- CostumerID int,
+-- CostumerName varchar(50),
+-- CostumerInfo varchar (50),
+-- );
 
- GO
-CREATE TRIGGER  trg_newCostumer 
+-- GO
+--CREATE TRIGGER  trg_newCostumer 
+--on Costumer 
+--After Insert 
+--as 
+--begin 
+--Insert into newCostumerAudit (CostumerID, CostumerName,CostumerInfo )
+--select CostumerID,
+--        CostumerName,
+--        CostumerContactInfo
+--from inserted 
+--end;
+
+--GO
+
+-- insert into Costumer 
+-- Values ('Yomna', '014')
+ 
+-- GO
+--select*
+-- from Costumer
+-- GO
+
+-- select *
+-- from newCostumerAudit
+
+ 
+-- insert into Costumer 
+-- Values ('Yasmeen', '014')
+ 
+-- GO
+
+--  select *
+-- from newCostumerAudit
+
+Go 
+create table AuditCostumerUpdate(
+AuditID int identity (1,1) Primary Key,
+CostumerID int,
+CostumerName varchar(50),
+CostumerContactInfo varchar(50)
+);
+
+Go 
+create trigger trg_CostumerUpdate
 on Costumer 
-After Insert 
-as 
-begin 
-Insert into newCostumerAudit (CostumerID, CostumerName,CostumerInfo )
-select CostumerID,
-        CostumerName,
-        CostumerContactInfo
-from inserted 
+after Update 
+as
+begin
+Insert into AuditCostumerUpdate (CostumerID,CostumerName, CostumerContactInfo)
+Select i.CostumerID, d.CostumerContactInfo, i.CostumerContactInfo
+from inserted i inner join deleted d on i.CostumerID = d.CostumerID
 end;
+Go 
 
-GO
+update Costumer 
+set CostumerContactInfo = '0111111'
+where CostumerID = 1151;
+Go 
+select *
+from AuditCostumerUpdate 
+GO 
 
- insert into Costumer 
- Values ('Yomna', '014')
- 
- GO
-select*
- from Costumer
- GO
+EXEC sp_rename 'AuditCostumerUpdate.CostumerName', 'OldContactInfo', 'COLUMN';
 
- select *
- from newCostumerAudit
+EXEC sp_rename 'AuditCostumerUpdate.CostumerContactInfo', 'NewContactInfo', 'COLUMN';
 
- 
- insert into Costumer 
- Values ('Yasmeen', '014')
- 
- GO
 
-  select *
- from newCostumerAudit
