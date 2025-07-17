f40fd0b8e614dae697524848c5583b766a454274
yomnaelgohary <yomnaelgohary011@gmail.com>
2025-07-17 10:48:31 +0300

    Deleted costumer trigger log in database

--- InsertTrigger.sql
+++ InsertTrigger.sql
@@ -46,36 +46,62 @@
 --  select *
 -- from newCostumerAudit
 
-Go 
-create table AuditCostumerUpdate(
-AuditID int identity (1,1) Primary Key,
-CostumerID int,
-CostumerName varchar(50),
-CostumerContactInfo varchar(50)
-);
+--Go 
+--create table AuditCostumerUpdate(
+--AuditID int identity (1,1) Primary Key,
+--CostumerID int,
+--CostumerName varchar(50),
+--CostumerContactInfo varchar(50)
+--);
 
-Go 
-create trigger trg_CostumerUpdate
-on Costumer 
-after Update 
-as
-begin
-Insert into AuditCostumerUpdate (CostumerID,CostumerName, CostumerContactInfo)
-Select i.CostumerID, d.CostumerContactInfo, i.CostumerContactInfo
-from inserted i inner join deleted d on i.CostumerID = d.CostumerID
-end;
-Go 
+--Go 
+--create trigger trg_CostumerUpdate
+--on Costumer 
+--after Update 
+--as
+--begin
+--Insert into AuditCostumerUpdate (CostumerID,CostumerName, CostumerContactInfo)
+--Select i.CostumerID, d.CostumerContactInfo, i.CostumerContactInfo
+--from inserted i inner join deleted d on i.CostumerID = d.CostumerID
+--end;
+--Go 
 
-update Costumer 
-set CostumerContactInfo = '0111111'
-where CostumerID = 1151;
-Go 
-select *
-from AuditCostumerUpdate 
-GO 
+--update Costumer 
+--set CostumerContactInfo = '0111111'
+--where CostumerID = 1151;
+--Go 
+--select *
+--from AuditCostumerUpdate 
+--GO 
 
-EXEC sp_rename 'AuditCostumerUpdate.CostumerName', 'OldContactInfo', 'COLUMN';
+--EXEC sp_rename 'AuditCostumerUpdate.CostumerName', 'OldContactInfo', 'COLUMN';
 
-EXEC sp_rename 'AuditCostumerUpdate.CostumerContactInfo', 'NewContactInfo', 'COLUMN';
+--EXEC sp_rename 'AuditCostumerUpdate.CostumerContactInfo', 'NewContactInfo', 'COLUMN';
 
 
+--GO 
+--create table AuditDeletedCostumer (
+--AuditID int identity (1,1) primary key,
+--CostumerName varchar(50),
+--CostumerContactInfo varchar(50)
+--);
+--GO
+--Create Trigger trg_DeletedCostumer 
+--on Costumer 
+--after Delete 
+--as 
+--begin 
+--Insert into AuditDeletedCostumer( CostumerName, CostumerContactInfo)
+--select CostumerName, CostumerContactInfo
+--from deleted 
+--end;
+--Go 
+--delete 
+--from Costumer
+--where CostumerID=161;
+--go 
+--select *
+--from AuditDeletedCostumer
+--go
+--select* 
+--from Costumer
